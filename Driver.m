df6d64f8bd64626bbaf4d5648b4bf1f90f63a98a
Kshitij Chhabra <31163910+kchhabra99@users.noreply.github.com>
2020-09-17 00:22:40 +0530

    Minor Changes

--- Driver.m
+++ Driver.m
@@ -1,24 +1,26 @@
 load_data
 Fs=512;
 channel=16;
-%spectData=S1(:,16);
-fAxis = 0:1/6:(Fs-1/6);
-L=6*Fs;
-%spectrumData = mean(abs(fft(spectData)),1);
-%figure; plot(fAxis,log10(spectrumData));
+L=6;
+fAxis = 0:1/L:(Fs-1/L);
+
+%Functions to determine Onset of the Stimulus
 eye_closed=eye_close(SIGNAL,Fs);
 eye_opened=eye_open(SIGNAL,Fs);
-spectData=zeros(3073,1);
-spectData2=zeros(3073,1);
+
+spectDataopen=zeros(3073,1);
+spectDataclose=zeros(3073,1);
 for i=1:5
-    spectData=spectData+S2(eye_opened(2):(eye_opened(2)+6*Fs),16);
-    spectData2=spectData2+S2(eye_closed(4):(eye_closed(4)+6*Fs),16);
+    spectDataopen=spectData+S2(eye_opened(2):(eye_opened(2)+6*Fs),channel);
+    spectDataclose=spectData2+S2(eye_closed(4):(eye_closed(4)+6*Fs),channel);
 end
-n=nextpow2(L);
-spectrumData = abs(fft(spectData/5));
-spectrumData2= abs(fft(spectData2/5));
+
+spectrumDataopen = abs(fft(spectData/5));
+spectrumDataclose= abs(fft(spectData2/5));
 
 hold on;
-plot(1:3073,log10(spectrumData),'red');
-plot(1:3073,log10(spectrumData2),'blue');
+plot(1:3073,log10(spectrumDataopen),'red');
+plot(1:3073,log10(spectrumDataclose),'blue');
+xlim([3 40]);
 hold off
+
