a9fa67fb68481686393e959de694b38a79fd0460
Kshitij Chhabra <31163910+kchhabra99@users.noreply.github.com>
2020-09-17 12:35:38 +0530

    Fixed Minor Bugs

--- Driver.m
+++ Driver.m
@@ -1,26 +1,31 @@
+%Loading the Data Sets
 load_data
+%Sampling Frequency=128Hz;
 Fs=512;
+%Channle to analayze=Oz
 channel=16;
+%Time for which we want the sample
 L=6;
+%Calulating the length of Frequency axis
 fAxis = 0:1/L:(Fs-1/L);
 
 %Functions to determine Onset of the Stimulus
 eye_closed=eye_close(SIGNAL,Fs);
 eye_opened=eye_open(SIGNAL,Fs);
 
-spectDataopen=zeros(3073,1);
-spectDataclose=zeros(3073,1);
+spectDataopen=zeros(3072,1);
+spectDataclose=zeros(3072,1);
 for i=1:5
-    spectDataopen=spectDataopen+S2(eye_opened(i):(eye_opened(i)+6*Fs),channel);
-    spectDataclose=spectDataclose+S2(eye_closed(i):(eye_closed(i)+6*Fs),channel);
+    spectDataopen=spectDataopen+S2(eye_opened(i):(eye_opened(i)+6*Fs-1),channel);
+    spectDataclose=spectDataclose+S2(eye_closed(i):(eye_closed(i)+6*Fs-1),channel);
 end
 
 spectrumDataopen = abs(fft(spectDataopen/5));
 spectrumDataclose= abs(fft(spectDataclose/5));
 
 hold on;
-plot(1:3073,log10(spectrumDataopen),'red');
-plot(1:3073,log10(spectrumDataclose),'blue');
+plot(fAxis,log10(spectrumDataopen),'red');
+plot(fAxis,log10(spectrumDataclose),'blue');
 xlim([3 40]);
 hold off
 
